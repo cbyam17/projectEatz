35354de72c0fc96b8a571ed046cee6a92ed1c286
Pops-Byam <78323261+Pops-Byam@users.noreply.github.com>
2021-04-16 17:35:51 -0400

    new Browse recipes page
    this is a description

--- js/add-recipe-helper.js
+++ js/add-recipe-helper.js
@@ -4,7 +4,29 @@
    Version: 1.0.1
 */
 
+
+
+
 $(document).ready(function () {
+	//
+	// JAVASCRIPT to show image icon on screen after selection
+	var imageLoader = document.getElementById('file-input');
+	imageLoader.addEventListener('change', handleImage, false);
+	var canvas = document.getElementById('imageCanvas');
+	var ctx = canvas.getContext('2d');
+
+	function handleImage(e){
+		var reader = new FileReader();
+		reader.onload = function(event){
+			var img = new Image();
+			img.onload = function(){
+				ctx.drawImage(img,0,0,100,100);
+			}
+			img.src = event.target.result;
+		}
+		reader.readAsDataURL(e.target.files[0]);     
+	}
+
 
     // function which is called when Div ID = addIngredient is clicked (AKA add Ingredient button)
     $("#addIngredient").on("click", function () {
@@ -46,6 +68,8 @@ $(document).ready(function () {
 		//serialize form data as formatted JSON object (this doesn't capture the picture)
 		var dataJSON = $('#addRecipeForm').serializeJSON();
 		var recipeID = '';
+		var cat = '';
+		
 		
 		var postUrl = 'https://d8qga9j6ob.execute-api.us-east-1.amazonaws.com/dev/recipe';
 		var authToken = '0eb6b64d-4aee-40d9-908d-4846044ee0f0';
@@ -53,6 +77,15 @@ $(document).ready(function () {
 		// Example POST method implementation:
 		async function postData(url = '', data = {}) {
 		  // Default options are marked with *
+
+
+		 // Temporary to get a file and put to s3
+		 // catElement= document.getElementById('category');
+		 // cat = catElement.value;
+		 //uploadPicture ("temp", cat);
+		 // return;
+		 //
+
 		  const answer = await fetch(url, {
 			method: 'POST', // *GET, POST, PUT, DELETE, etc.
 			mode: 'cors', // no-cors, *cors, same-origin
@@ -70,6 +103,7 @@ $(document).ready(function () {
 		  .then (data => {
 			alert('Successful add of recipe!');
 			recipeID = data.recipeId;
+			cat = document.getElementById('category').value;
 		  })
 		  .catch(error => {
 			alert('Failure to add recipe');
@@ -78,13 +112,58 @@ $(document).ready(function () {
 
 		result = postData(postUrl, dataJSON)
 		  .then(data => {
-			uploadPicture(recipeID); // JSON data parsed by `data.json()` call
+			uploadPicture(recipeID, cat); 
 		  });
 		
 		//get response from this post, save the recipeId returned, and then add to s3 with the name of that id
-		function uploadPicture(recipeID){
+		function uploadPicture(recipeId, category){
+			// Initialize the Amazon Cognito credentials provider
+			var recipeBucketName = 'my-recipe-pictures';
+			AWS.config.region = 'us-east-2'; // Region
+			AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    		IdentityPoolId: 'us-east-2:e4c9746c-7ccc-487d-9e97-97771e73a0f2',});
+
+			// Create a new service object
+			var s3 = new AWS.S3({
+ 			 	apiVersion: '2006-03-01',
+  				params: {Bucket: recipeBucketName}
+			});
+			
 			if (recipeID !==  '') {
-				alert('Got to loading picture =' + recipeID);
+				var files = document.getElementById("file-input").files;
+				if (!files.length) {
+				  return alert("Error - can't access file");
+				}
+				var file = files[0];
+				var fileName = file.name;
+				var categoryKey = encodeURIComponent(category) + "/";
+			  
+				var recipeKey = categoryKey + recipeId;
+			  
+				// Use S3 ManagedUpload class as it supports multipart uploads
+				var upload = new AWS.S3.ManagedUpload({
+				  params: {
+					Bucket: recipeBucketName,
+					Key: recipeKey,
+					Body: file,
+					ACL:'public-read'
+				  }
+				});
+			  
+				var promise = upload.promise();
+				promise.then(
+				  function(data) {
+					alert("Successfully uploaded photo.");
+					window.location.href = "index.html";
+				  },
+				  function(err) {
+					return alert("There was an error uploading your photo: ", err.message);
+				  }
+				);
+
+			} else {
+				window.location.href = "index.html";
+
 			}
 		}
 				
